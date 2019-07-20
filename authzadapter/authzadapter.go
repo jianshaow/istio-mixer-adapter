5e6429f707eb81aefee5c1bffa37e801968774d0
Jianshao <jms_wu@hotmail.com>
2019-07-20 12:24:12 +0800

    add code skeleton

---
+++ authzadapter/authzadapter.go
@@ -0,0 +1,75 @@
+package authzadapter
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"istio.io/api/mixer/adapter/model/v1beta1"
+	"istio.io/istio/mixer/template/authorization"
+)
+
+type (
+	// Server is basic server interface
+	Server interface {
+		Addr() string
+		Close() error
+		Run(shutdown chan error)
+	}
+
+	// AuthzAdapter supports authorization template.
+	AuthzAdapter struct {
+		listener net.Listener
+		server   *grpc.Server
+	}
+)
+
+var _ authorization.HandleAuthorizationServiceServer = &AuthzAdapter{}
+
+// HandleAuthorization handler the request
+func (s *AuthzAdapter) HandleAuthorization(ctx context.Context, r *authorization.HandleAuthorizationRequest) (*v1beta1.ReportResult, error) {
+	return nil, nil
+}
+
+// Addr returns the listening address of the server
+func (s *AuthzAdapter) Addr() string {
+	return s.listener.Addr().String()
+}
+
+// Run starts the server run
+func (s *AuthzAdapter) Run(shutdown chan error) {
+	shutdown <- s.server.Serve(s.listener)
+}
+
+// Close gracefully shuts down the server; used for testing
+func (s *AuthzAdapter) Close() error {
+	if s.server != nil {
+		s.server.GracefulStop()
+	}
+
+	if s.listener != nil {
+		_ = s.listener.Close()
+	}
+
+	return nil
+}
+
+// NewAuthzAdapter creates a new IBP adapter that listens at provided port.
+func NewAuthzAdapter(addr string) (Server, error) {
+	if addr == "" {
+		addr = "0"
+	}
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", addr))
+	if err != nil {
+		return nil, fmt.Errorf("unable to listen on socket: %v", err)
+	}
+	s := &AuthzAdapter{
+		listener: listener,
+	}
+	fmt.Printf("listening on \"%v\"\n", s.Addr())
+	s.server = grpc.NewServer()
+	authorization.RegisterHandleAuthorizationServiceServer(s.server, s)
+	return s, nil
+}
--- authzadapter/cmd/main.go
+++ authzadapter/cmd/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	authzadapter "istio.io/istio/mixer/adapter/authzadapter"
+)
+
+func main() {
+	addr := ""
+	if len(os.Args) > 1 {
+		addr = os.Args[1]
+	}
+
+	s, err := authzadapter.NewAuthzAdapter(addr)
+	if err != nil {
+		fmt.Printf("unable to start server: %v", err)
+		os.Exit(-1)
+	}
+
+	shutdown := make(chan error, 1)
+	go func() {
+		s.Run(shutdown)
+	}()
+	_ = <-shutdown
 }
